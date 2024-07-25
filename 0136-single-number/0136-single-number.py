621dcd6c6083bdcc6e17fae734ec1f1dc5eaef70
rahulhalder123-456 <105984283+rahulhalder123-456@users.noreply.github.com>
2024-07-25 16:30:49 +0530

    Time: 119 ms (23.73%), Space: 20 MB (7%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,16 +1,12 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # seen = set()
-        # duplicate = set()
-        # for num in nums:
-        #     if num in seen:
-        #         duplicate.add(num)
-        #     else:
-        #         seen.add(num)
-        # for num in nums:
-        #     if num not in duplicate:
-        #         return num
-        single = 0
+        seen = set()
+        duplicate = set()
         for num in nums:
-            single ^= num
-        return single
+            if num in seen:
+                duplicate.add(num)
+            else:
+                seen.add(num)
+        for num in nums:
+            if num not in duplicate:
+                return num
